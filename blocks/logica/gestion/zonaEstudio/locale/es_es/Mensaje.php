dc87543915cabd46e098bc5057a9e6a873196f42
Stiv <stivmarquez.ud@gmail.com>
2015-11-11 23:05:17 -0800

    Modificación Bloque Gestión Zona Estudio

---
+++ blocks/logica/gestion/zonaEstudio/locale/es_es/Mensaje.php
@@ -0,0 +1,226 @@
+<?php
+# Caso de Uso 
+
+//Pestañas
+$this->idioma["tabDescripcion"]="Descripción General Zona de Estudio";
+$this->idioma["tabAnalisis"]="Analisis de Variables";
+$this->idioma["tabResultado"]="Resultado o Informe ";
+
+
+//Nombre Formulario
+
+$this->idioma["gestionRiesgo"]="Gestión de Riesgo a la Navegación";
+
+//Mis Campos
+$this->idioma["region"]="Región : ";
+$this->idioma["regionTitulo"]="Seleccione Región";
+
+$this->idioma["sector"]="Sector : ";
+$this->idioma["sectorTitulo"]="Seleccione Sector";
+
+
+//Buques Comerciales
+
+$this->idioma["rango1BC"]="Cantidad Buques Mayor a 10.000GT : ";
+$this->idioma["rango1BCTitulo"]="Ingrese Número de Buques Comerciales Mayores a 10.000GT";
+
+$this->idioma["tiempo1BC"]="Periodo : ";
+$this->idioma["tiempo1BCTitulo"]="Seleccione Período";
+
+$this->idioma["rango2BC"]="Cantidad Buques entre 9.999 y 1.000GT : ";
+$this->idioma["rango2BCTitulo"]="Ingrese Número de Buques Comerciales Entre 9.999 y 1.000GT";
+
+$this->idioma["tiempo2BC"]="Periodo : ";
+$this->idioma["tiempo2BCTitulo"]="Seleccione Período";
+
+$this->idioma["rango3BC"]="Cantidad Buques Menores  a  1.000GT : ";
+$this->idioma["rango3BCTitulo"]="Ingrese Número de Buques Comerciales Menores a 1.000GT";
+
+$this->idioma["tiempo3BC"]="Periodo : ";
+$this->idioma["tiempo3BCTitulo"]="Seleccione Período";
+
+// Buques de Energía
+
+$this->idioma["rango1BE"]="Cantidad Buques VLCCs/ULCCs : ";
+$this->idioma["rango1BETitulo"]="Ingrese Número de Buques VLCCs/ULCCs";
+
+$this->idioma["tiempo1BE"]="Periodo : ";
+$this->idioma["tiempo1BETitulo"]="Seleccione Período";
+
+$this->idioma["rango2BE"]="Cantidad Buques LNG : ";
+$this->idioma["rango2BETitulo"]="Ingrese Número de Buques LNG";
+
+$this->idioma["tiempo2BE"]="Periodo : ";
+$this->idioma["tiempo2BETitulo"]="Seleccione Período";
+
+//Buques Pasajeros
+
+$this->idioma["rango1BP"]="Cantidad Buques Transbordadores Regionales : ";
+$this->idioma["rango1BPTitulo"]="Ingrese Número de Buques Transbordadores Regionales";
+
+$this->idioma["tiempo1BP"]="Periodo : ";
+$this->idioma["tiempo1BPTitulo"]="Seleccione Período";
+
+$this->idioma["rango2BP"]="Cantidad Buques Ferris Rápidos : ";
+$this->idioma["rango2BPTitulo"]="Ingrese Número de Buques Ferris Rápidos";
+
+$this->idioma["tiempo2BP"]="Periodo : ";
+$this->idioma["tiempo2BPTitulo"]="Seleccione Período";
+
+$this->idioma["rango3BP"]="Cantidad Buques Buques de Crucero : ";
+$this->idioma["rango3BPTitulo"]="Ingrese Número de Buques de Crucero";
+
+$this->idioma["tiempo3BP"]="Periodo : ";
+$this->idioma["tiempo3BPTitulo"]="Seleccione Período";
+
+
+//Buques Guerra
+
+$this->idioma["rango1BG"]="Cantidad Buques Guerra Desplazando> 5.000 t :";
+$this->idioma["rango1BGTitulo"]="Ingrese Número de Buques Guerra Desplazando> 5.000 t";
+
+$this->idioma["tiempo1BG"]="Periodo : ";
+$this->idioma["tiempo1BGTitulo"]="Seleccione Período";
+
+$this->idioma["rango2BG"]="Cantidad Buques Guerra desplazando< 5.000 t :";
+$this->idioma["rango2BGTitulo"]="Ingrese Número de Buques de Guerra desplazando< 5.000 t";
+
+$this->idioma["tiempo2BG"]="Periodo : ";
+$this->idioma["tiempo2BGTitulo"]="Seleccione Período";
+
+//Buques Pesqueros
+
+$this->idioma["rango1BPQ"]="Cantidad Buques Pesqueros > 100 GT :";
+$this->idioma["rango1BPQTitulo"]="Ingrese Número de Buques Pesqueros > 100 GT";
+
+$this->idioma["tiempo1BPQ"]="Periodo : ";
+$this->idioma["tiempo1BPQTitulo"]="Seleccione Período";
+
+//Servicios Marítimos
+
+$this->idioma["rango1SM"]="Cantidad Buques de Investigación :";
+$this->idioma["rango1SMTitulo"]="Ingrese Número de Buques de Investigación";
+
+$this->idioma["tiempo1SM"]="Periodo : ";
+$this->idioma["tiempo1SMTitulo"]="Seleccione Período";
+
+$this->idioma["rango2SM"]="Cantidad Buques de Apoyo Aparejan :";
+$this->idioma["rango2SMTitulo"]="Ingrese Número de  Buques de Apoyo Aparejan";
+
+$this->idioma["tiempo2SM"]="Periodo : ";
+$this->idioma["tiempo2SMTitulo"]="Seleccione Período";
+
+$this->idioma["rango3SM"]="Cantidad Botes de Pilotos :";
+$this->idioma["rango3SMTitulo"]="Ingrese Número de  Botes de Pilotos";
+
+$this->idioma["tiempo3SM"]="Periodo : ";
+$this->idioma["tiempo3SMTitulo"]="Seleccione Período";
+
+$this->idioma["rango4SM"]="Cantidad Remolcadores :";
+$this->idioma["rango4SMTitulo"]="Ingrese Número de  Remolcadores";
+
+$this->idioma["tiempo4SM"]="Periodo : ";
+$this->idioma["tiempo4SMTitulo"]="Seleccione Período";
+
+$this->idioma["rango5SM"]="Cantidad Otros :";
+$this->idioma["rango5SMTitulo"]="Ingrese Número Otros Tipos";
+
+$this->idioma["tiempo5SM"]="Periodo : ";
+$this->idioma["tiempo5SMTitulo"]="Seleccione Período";
+
+//Acua-Avíones Privados
+
+$this->idioma["rango1AA"]="Cantidad de Lanchas y Yates > 100 GT :";
+$this->idioma["rango1AATitulo"]="Ingrese Número de Lanchas y Yates > 100 GT";
+
+$this->idioma["tiempo1AA"]="Periodo : ";
+$this->idioma["tiempo1AATitulo"]="Seleccione Período";
+
+$this->idioma["rango2AA"]="Cantidad de Lanchas y Yates < 100 GT :";
+$this->idioma["rango2AATitulo"]="Ingrese Número de  Lanchas y Yates < 100 GT";
+
+$this->idioma["tiempo2AA"]="Periodo : ";
+$this->idioma["tiempo2AATitulo"]="Seleccione Período";
+
+$this->idioma["rango3AA"]="Cantidad de Embarcaciones de Recreo Pequeño :";
+$this->idioma["rango3AATitulo"]="Ingrese Número de  Embarcaciones de Recreo Pequeño";
+
+$this->idioma["tiempo3AA"]="Periodo : ";
+$this->idioma["tiempo3AATitulo"]="Seleccione Período";
+
+$this->idioma["rango4AA"]="Cantidad de buques de Vela :";
+$this->idioma["rango4AATitulo"]="Ingrese Número de  Los buques de Vela";
+
+$this->idioma["tiempo4AA"]="Periodo : ";
+$this->idioma["tiempo4AATitulo"]="Seleccione Período";
+
+
+
+
+//Tamaño de Buques
+
+$this->idioma["num_bq_gr"]="Máximo Número de Buques Grandes :";
+$this->idioma["num_bq_grTitulo"]="Ingrese Número Máximo de Buques Grandes";
+
+$this->idioma["tiempo_bq_gr"]="Periodo : ";
+$this->idioma["tiempo_bq_grTitulo"]="Seleccione Período";
+
+$this->idioma["num_bq_pq"]="Máximo Número de Buques Pequeños :";
+$this->idioma["num_bq_pqTitulo"]="Ingrese Número Maxímo de Buques Pequeños";
+
+$this->idioma["tiempo_bq_pq"]="Periodo : ";
+$this->idioma["tiempo_bq_pqTitulo"]="Seleccione Período";
+
+//Configuración Hidrovia
+
+
+$this->idioma["pr_co_ba"]="Profundidad / Corriente de aire / Bajo la Quilla (mt) : ";
+$this->idioma["tiempo_bq_pqTitulo"]="Ingrese Profundidad / Corriente de aire / Bajo la Quilla (Metros)";
+
+$this->idioma["ancho_canal"]="Ancho de Canal  (Km) : ";
+$this->idioma["ancho_canalTitulo"]="Ingrese Ancho de Canal (Kilometros)";
+
+$this->idioma["obtrucciones_visibilidad"]="Obstrucciones de Visibilidad : ";
+$this->idioma["obtrucciones_visibilidadTitulo"]="Ingrese Obstrucciones de Visibilidad";
+
+$this->idioma["complejidad_hidrovia"]="Complejidad Hidrovía : ";
+$this->idioma["complejidad_hidroviaTitulo"]="Ingrese Complejidad Hidrovía";
+
+$this->idioma["tipo_fondo"]="Tipo de Fondo : ";
+$this->idioma["tipo_fondoTitulo"]="Ingrese Tipo de Fondo(Suelo)";
+
+$this->idioma["estabilidad_sedimentos"]="Estabilidad Sedimentos : ";
+$this->idioma["estabilidad_sedimentosTitulo"]="Ingrese Estabilidad Sedimentos";
+
+$this->idioma["estabilidad_sedimentos"]="Estabilidad Sedimentos : ";
+$this->idioma["estabilidad_sedimentosTitulo"]="Ingrese Estabilidad Sedimentos";
+
+$this->idioma["ayudas_navegacion"]="Ayudas a la Navegación Mezcla y Configuración : ";
+$this->idioma["ayudas_navegacionTitulo"]="Ingrese Ayudas a la Navegación Mezcla y Configuración";
+
+$this->idioma["calidad_datos"]="Calidad de Datos Hidrográficos : ";
+$this->idioma["calidad_datosTitulo"]="Ingrese Calidad de Datos Hidrográficos";
+
+
+
+
+/*
+$this->idioma["rango3BG"]="Cantidad Buques Buques de Crucero : ";
+$this->idioma["rango3BGTitulo"]="Ingrese Número de Buques de Crucero";
+
+$this->idioma["tiempo3BG"]="Periodo : ";
+$this->idioma["tiempo3BG	Titulo"]="Seleccione Período";
+*/
+
+//-------------------------------------
+
+$this->idioma["usuario"]="Identificación:";
+$this->idioma["clave"]="Clave:";
+$this->idioma["usuarioTitulo"]="Número de documento";
+$this->idioma["claveTitulo"]="Clave de Acceso";
+$this->idioma["botonAceptar"]="Aceptar";
+$this->idioma["botonCancelar"]="Cancelar";
+$this->idioma["noDefinido"]="No definido";
+$this->idioma["botonIngresar"]="Ingresar";
+
+?>
