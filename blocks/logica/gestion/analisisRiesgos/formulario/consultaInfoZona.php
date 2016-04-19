f9ab6f4195286147631dc14d860a015fc9f61891
Stiv <stivmarquez.ud@gmail.com>
2016-04-18 17:06:48 -0700

    Adición Menu de de Informe de Resultados

--- blocks/logica/gestion/analisisRiesgos/formulario/consultaInfoZona.php
+++ blocks/logica/gestion/analisisRiesgos/formulario/consultaInfoZona.php
@@ -75,7 +75,7 @@ class consultarForm {
 		$atributos ['id'] = $esteCampo;
 		$atributos ["estilo"] = "jqueryui";
 		$atributos ['tipoEtiqueta'] = 'inicio';
-		$atributos ["leyenda"] = "Consulta Proyecto de la Zona de Estudio : ";
+		$atributos ["leyenda"] = "Analisis Variables<br>Consulta Proyecto de la Zona de Estudio : ";
 		echo $this->miFormulario->marcoAgrupacion ( 'inicio', $atributos );
 		unset ( $atributos );
 		
